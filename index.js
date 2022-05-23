d8aff40c0c3138dc669e8ab13e1a1f0f1b973e6f
TayeburRahman <tayebrayhan101@gmail.com>
2022-05-24 01:56:24 +0600

    heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 //* FIREBASE INITIALZE APP
 const admin = require("firebase-admin");
